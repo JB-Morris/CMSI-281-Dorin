33bab0287afc4538e30513a94b9cbf0b767b237d
jb0001994 <jb0001994@aol.com>
2015-10-29 17:32:49 -0700

    first draft complete will need to double check methods

--- Homework 3/LinkedDeque.java
+++ Homework 3/LinkedDeque.java
@@ -1,3 +1,9 @@
+import java.lang.IllegalArgumentException;
+import java.lang.Exception;
+import java.lang.IllegalStateException;
+import java.util.Arrays;
+import java.lang.StringBuilder;
+
 public class LinkedDeque<T> {
 
 	private int size;
@@ -11,39 +17,114 @@ public class LinkedDeque<T> {
 	}	
 
 	public void insertLeft (T o) {
-		throw new UnsupportedOperationException();
+		Node<T> newNode = new Node<T>(o);
+
+		if(this.size == 0){
+			this.head = newNode;
+			this.current = this.head;
+			this.size++;
+		}else{
+			this.head.setPrevious(newNode);
+			this.head.getPrevious().setNext(this.head);
+			this.head = this.head.getPrevious();
+			this.size++;
+		}
+		
+		// throw new UnsupportedOperationException();
 	}
 
 	public void insertRight (T o) {
-		throw new UnsupportedOperationException();
+		Node<T> newNode = new Node<T>(o);
+
+		if(this.size == 0){
+			this.head = newNode;
+			this.current = this.head;
+			this.size++;
+		}else{
+			newNode.setPrevious(this.current);
+			this.current.setNext(newNode);
+			this.current = this.current.getNext();
+			this.size++;
+
+		}
+		// throw new UnsupportedOperationException();
 	}
 
 	public void deleteLeft() {
-		throw new UnsupportedOperationException();
+		if(this.head == null){
+			throw new IllegalStateException("Cannot delete from an empty list.");
+		}
+		if(this.head.hasNext()){
+			this.head = this.head.getNext();
+			this.head.setPrevious(null);
+			this.size--;
+		}else{
+			this.head = null;
+			this.current = null;
+			this.size = 0;
+		}
+		
+		// throw new UnsupportedOperationException();
 	}
 
 	public void deleteRight() {
-		throw new UnsupportedOperationException();
+		if(this.head == null){
+			throw new IllegalStateException("Cannot delete from an empty list.");
+		}
+		if(this.size == 1){
+			this.head = null;
+			this.current = null;
+			this.size--;
+		}else{
+			this.current = this.current.getPrevious();
+			this.current.setNext(null);
+			this.size--;
+		}
+
+		// throw new UnsupportedOperationException();
 	}
 
 	public T left() {
-		throw new UnsupportedOperationException();
+		return this.head.getValue();
+		// throw new UnsupportedOperationException();
 	}
 
 	public T right() {
-		throw new UnsupportedOperationException();
+		return this.current.getValue();
+		// throw new UnsupportedOperationException();
 	}
 
 	public int size() {
-		throw new UnsupportedOperationException();
+		return this.size;
+		// throw new UnsupportedOperationException();
 	}
 
 	public String toString() {
-		throw new UnsupportedOperationException();
+
+		StringBuilder output = new StringBuilder("[");
+		Node<T> stringNode = this.head;
+		for(int i = 0; i < this.size; i++){
+			output.append(stringNode.getValue().toString());
+			if(i < this.size - 1){
+				output.append(", ");
+			}
+			stringNode = stringNode.getNext();
+		}
+		output.append("]");
+		return output.toString();
+
+		// throw new UnsupportedOperationException();
 	}
 
 	public static void main(String[] args) {
-		throw new UnsupportedOperationException();
+
+		LinkedDeque<Integer> lDeque = new LinkedDeque<Integer>();
+		lDeque.insertLeft(5);
+		lDeque.insertLeft(6);
+		lDeque.insertRight(4);
+		lDeque.deleteRight();
+		System.out.println(lDeque.toString());
+		// throw new UnsupportedOperationException();
 	}
 
 	private class Node<T> {
@@ -78,6 +159,13 @@ public class LinkedDeque<T> {
 
 		public void setPrevious(Node<T> previous) {
 			this.previous = previous;
+		}
+
+		public boolean hasNext(){
+			if(this.next == null){
+				return false;
+			}
+			return true;
 		}
 
 	}
